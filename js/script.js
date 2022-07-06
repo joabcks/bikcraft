de7a2ffbe4d982a0f968c3b59be1f0469df88860
joabcks <joabcks@gmail.com>
2022-07-06 00:27:50 -0300

    adicionada funcao js ativar itens do orcamento

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Ativar links do Menu
 const links = document.querySelectorAll('.header-menu a');
 
 function ativarLink(link) {
@@ -7,7 +8,19 @@ function ativarLink(link) {
   if (url.includes(href)) {
     link.classList.add("ativo");
   }
+}
+links.forEach(ativarLink);
+
+//Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
 
 }
 
-links.forEach(ativarLink);
+parametros.forEach(ativarProduto);
